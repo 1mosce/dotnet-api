91901943d439d784c3d7a57d43cb814dfd909089
1mosce <shuliaknikolay@gmail.com>
2022-05-07 20:52:19 +0300

    First Commit, Api Init, Item GET method added

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Catalog.Entities;
+using System;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository 
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 12, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Metal Armor", Price = 30, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }  
+}
